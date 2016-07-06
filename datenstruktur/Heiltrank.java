035bee45e9334d516d64b52c93f305af8ccabbce
User <User@User-PC.fritz.box>
2016-07-06 20:11:51 +0200

    Datenstruktur geändert

--- datenstruktur/Heiltrank.java
+++ datenstruktur/Heiltrank.java
@@ -1,10 +1,13 @@
 package datenstruktur;
 
-public class Heiltrank {
+public class Heiltrank extends Spielelement{
+	
 	private int wirkung;
 	
-	public Heiltrank(int wirkung){
+	public Heiltrank(int wirkung,int x,int y){
 		this.wirkung = wirkung;
+		this.posx=x;
+		this.posy=y;
 	}
 	
 	public int getWirkung(){
