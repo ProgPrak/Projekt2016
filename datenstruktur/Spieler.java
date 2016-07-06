7fde11e60ed2ad89b7de05bacbd32675cd0bee9c
Jabo <Jabo@PC>
2016-07-06 18:26:37 +0200

    blabla

--- datenstruktur/Spieler.java
+++ datenstruktur/Spieler.java
@@ -8,9 +8,9 @@ import java.io.IOException;
 import javax.imageio.ImageIO;
 //Jabo
 public class Spieler extends Figur {
-// Kommentar
+// Kommentar1234
 	private String name;
-
+//asdkjlas
 	private boolean hatSchluessel;
 	private int anzHeiltraenke;
 	private int heiltrankWirkung;
